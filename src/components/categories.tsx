9b716b852bc278b18fe8e54e69eb639eb1cfec05
CoolChris21 <christianr74@live.com>
2024-06-07 11:20:16 +0200

    refactor: Update import paths for header and categories components

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -7,21 +7,24 @@ import virtual from "../images/virtual.webp";
 import esport from "../images/esport.jpeg";
 import live from "../images/live.jpeg";
 
+
 const Categories: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center my-2">
       <ul className="flex justify-center space-x-4">
         <li>
-          <button className="relative w-24 h-24 rounded-3xl overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-200">
-            <div className="absolute inset-0">
-              <Image
-                src={sport}
-                alt="Sport"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <span className="relative text-white z-10 font-bold">Sport</span>
-          </button>
+          <Link href="/categoriesPage">
+            <button className="relative w-24 h-24 rounded-3xl overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-200">
+              <div className="absolute inset-0">
+                <Image
+                  src={sport}
+                  alt="Sport"
+                  className="object-cover w-full h-full"
+                />
+              </div>
+              <span className="relative text-white z-10 font-bold">Sport</span>
+            </button>
+          </Link>
         </li>
         <li>
           <button className="relative w-24 h-24 rounded-3xl overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-200">
@@ -44,7 +47,9 @@ const Categories: React.FC = () => {
                 className="object-cover w-full h-full"
               />
             </div>
-            <span className="relative text-white z-10 font-bold">Virtual & XR</span>
+            <span className="relative text-white z-10 font-bold">
+              Virtual & XR
+            </span>
           </button>
         </li>
         <li className="sm:flex hidden">
@@ -68,11 +73,13 @@ const Categories: React.FC = () => {
                 className="object-cover w-full h-full"
               />
             </div>
-            <span className="relative text-white z-10 font-bold">Live Production</span>
+            <span className="relative text-white z-10 font-bold">
+              Live Production
+            </span>
           </button>
         </li>
       </ul>
-      <Link href="/categoriesPage">
+      <Link href="/allProductPage">
         <button className="my-8 px-4 py-2 text-base font-medium text-white rounded-md bg-gradient-to-r from-orange-400 to-orange-700 shadow-lg transform hover:scale-105 transition-transform duration-200">
           View all Products
         </button>
