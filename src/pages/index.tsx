a630492624104e7cab425d0fe8d785780733ec11
CoolChris21 <christianr74@live.com>
2024-06-07 10:53:39 +0200

    chore: Remove temporary link to Premier League package page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,12 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Categories from "../components/categories";
 import CarouselItems from "../components/CarouselItems";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <Header />
       <Categories />
-
-      <Link href="/premierLeaguePage">
-        <p className="text-white text-center font-medium">Premier League package (midlertidlig link)</p>
-      </Link>
-
       <CarouselItems />
       <Footer />
     </div>
