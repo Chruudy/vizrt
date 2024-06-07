018a9bb1b893560e9348b89a3ae22a280ec6f306
CoolChris21 <christianr74@live.com>
2024-06-07 10:27:51 +0200

    refactor: Update header component markup and styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,7 @@ import StarIcon from "@mui/icons-material/Star";
 import Image from "next/image";
 import Logo from "../images/Vizrt-Logo-Orange.webp";
 import Link from "next/link";
+import CreateIcon from '@mui/icons-material/Create';
 
 const Header: React.FC = () => {
   return (
@@ -14,17 +15,17 @@ const Header: React.FC = () => {
       <Link href="/">
         <Image src={Logo} alt="Site Icon" className="w-20 mx-16 my-4" />
       </Link>
-      <div className="relative text-white w-1/3 mx-6">
+      <div className="relative text-white w-1/3 ml-32">
         <input
           type="text"
-          className="pr-8 pl-3 py-1 rounded bg-white text-black w-full text-center"
+          className=" py-1 rounded bg-white text-black w-full text-center"
           placeholder="search"
         />
         <button className="absolute left-0 top-0 mt-1">
           <SearchIcon className="text-black mx-2" />
         </button>
       </div>
-      <ul className="flex items-center text-white mr-6">
+      <ul className="flex items-center text-white mr-2">
         <li>
           <button className="transition-colors duration-400 ease-in-out hover:bg-orange-500 hover:text-black h-20 w-20">
             <MenuIcon />
@@ -39,11 +40,11 @@ const Header: React.FC = () => {
         </li>
         <li>
           <Link href="/login">
-            <button className="transition-colors duration-400 ease-in-out hover:bg-orange-500 hover:text-black h-20 w-20">
-              <Person2Icon />
-            </button>
+          <button className="transition-colors duration-400 ease-in-out hover:bg-orange-500 hover:text-black h-20 w-20">
+            <Person2Icon />
+          </button>
           </Link>
-        </li>
+        </li>  
         <li>
           <Link href="/uploadtest">
             <button className="transition-colors duration-400 ease-in-out hover:bg-orange-500 hover:text-black h-20 w-20">
@@ -51,6 +52,7 @@ const Header: React.FC = () => {
             </button>
           </Link>
         </li>
+
       </ul>
     </header>
   );
