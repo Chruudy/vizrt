62e8e7e2e1d84166405df0ff1e7bacdd4cd30ebb
kevin <kevin_puvipal@hotmail.com>
2024-06-10 20:27:55 +0200

    small changes on profile info page

--- src/components/profileInfo.tsx
+++ src/components/profileInfo.tsx
@@ -6,38 +6,27 @@ import profilePic from '../images/profilepic.jpeg'; // Adjust the import path as
 
 const ProfileInfo: React.FC = () => {
   return (
-    <div className="relative">
-      <div className="flex items-center space-x-4 p-6 bg-gray-800">
-        <div className="relative flex-shrink-0 h-32 w-32 rounded-full overflow-hidden bg-gray-200 border-4 border-white shadow-lg">
-          <div className="absolute inset-0 border-4 border-white rounded-full"></div>
-          <Image src={profilePic} alt="Profile Picture" layout="fill" objectFit="cover" />
-          <button 
-            className="absolute bottom-0 right-0 bg-white p-1 rounded-full border-2 border-gray-300 flex items-center justify-center w-8 h-8 text-gray-800 hover:bg-gray-300"
-            onClick={() => alert('Upload image functionality will be here')}>
-            <span className="text-xl font-bold">+</span>
-          </button>
-        </div>
-        <div>
-          <h2 className="font-bold text-white">Nicole Frost</h2>
-          <p className="font-bold text-white">Product Designer</p>
-          <p className="font-bold text-white">San Francisco</p>
-          <div className="flex space-x-4 mt-2 text-white">
-            <div>
-              <span className="font-bold">200</span> followers
-            </div>
-            <div>
-              <span className="font-bold">100</span> following
-            </div>
+    <div className="flex items-center space-x-4">
+      <div className="relative flex-shrink-0 h-32 w-32 rounded-full overflow-hidden shadow-lg">
+        <Image
+          src={profilePic}
+          alt="Profile Picture"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+      <div>
+        <h2 className="font-bold text-white">Nicole Frost</h2>
+        <p className="font-bold text-white">Product Designer</p>
+        <p className="font-bold text-white">San Francisco</p>
+        <div className="flex space-x-4 mt-2 text-white">
+          <div>
+            <span className="font-bold">200</span> followers
+          </div>
+          <div>
+            <span className="font-bold">100</span> following
           </div>
         </div>
-      </div>
-      
-      {/* White Line Divider */}
-      <div className="absolute left-0 right-0 h-1 bg-white" style={{ top: '50%' }}></div>
-
-      {/* Rest of the content */}
-      <div className="mt-6 p-6 bg-gray-800">
-        {/* Your other content goes here */}
       </div>
     </div>
   );
