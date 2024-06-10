194009ad61cd7bd62c6c83160315dfc2016df965
kevin <kevin_puvipal@hotmail.com>
2024-06-10 20:25:44 +0200

    moved on the edit account button

--- src/components/editAccount.tsx
+++ src/components/editAccount.tsx
@@ -1,10 +1,9 @@
-// components/EditAccountButton.tsx
 import React from 'react';
 
 const EditAccountButton: React.FC = () => {
   return (
-    <div className="text-center mt-4">
-      <button className="bg-orange-500 text-white py-2 px-4 rounded">
+    <div className="text-left w-full">
+      <button className="bg-orange-500 text-white py-1 px-3 rounded mt-4 text-sm">
         Edit Account
       </button>
     </div>
