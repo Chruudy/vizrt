acd99dce0375bda4129d6a3238d2e735fcb82a14
kevin <kevin_puvipal@hotmail.com>
2024-06-12 10:44:13 +0200

    Added title

--- src/pages/categoriesPage.tsx
+++ src/pages/categoriesPage.tsx
@@ -4,18 +4,21 @@ import CarouselItemsCategories from "@/components/CarouselItemsCategories";
 import CategoryBoxes from "@/components/CategoryBoxes"; // Importing the CategoryBoxes component
 import Categories from "@/components/categories";
 import CarouselCategories from "@/components/CarouselCategories";
+
 const CategoriesPage = () => {
   return (
     <div className="overflow-x-hidden">
-      
       <Header />
-      
       <Categories />
+      <div className="text-center my-10">
+        <h2 className="text-5xl font-bold text-white">Sub Categories</h2>
+        <div className="mt-2 border-b-4 border-orange-500 w-1/4 mx-auto"></div>
+      </div>
       <div className="mb-10">
-       <CarouselCategories/>
+        <CarouselCategories />
       </div>
       <div className="container mx-auto p-5">
-        <CategoryBoxes /> 
+        <CategoryBoxes />
       </div>
       <Footer />
     </div>
